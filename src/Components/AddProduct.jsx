30e73de7551c1b58087a8a559614d7d2422f7c2b
chand-stack <chand.stack@gmail.com>
2023-10-18 19:50:33 +0600

    Chore: products card section added

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -1,6 +1,46 @@
+import Swal from 'sweetalert2';
 import bg from '../assets/carcover2.jpg'
 
 const AddProduct = () => {
+
+const addProduct = e => {
+    e.preventDefault()
+    const form = e.target 
+    const name = form.name.value 
+    const brandName = form.brand.value
+    const brandNameLower = brandName.toLowerCase()
+    const type = form.type.value 
+    const price = form.price.value 
+    const description = form.description.value 
+    const rating = form.rating.value 
+    const photo = form.photo.value 
+    console.log(name,brandNameLower,type,price,description,rating,photo);
+    const car = {
+        name,brandName,type,price,description,rating,photo
+    }
+
+    fetch(`http://localhost:5000/${brandNameLower}`,{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(car)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.insertedId){
+            Swal.fire(
+                'Good job!',
+                'Product added successfully!',
+                'success'
+              )
+              e.target.reset()
+        }
+    })
+
+}
+
     return (
         <>
         <div className='h-[50vh]' style={{backgroundImage:`url(${bg})`,backgroundSize:"cover",backgroundRepeat:"no-repeat", backgroundPosition:"center"}}>
@@ -10,39 +50,39 @@ const AddProduct = () => {
         </div>
 
 <div className='container mx-auto py-10 px-3 md:px-10 lg:px-16'>
-    <form className='space-y-4'>
+    <form onSubmit={addProduct} className='space-y-4'>
 
 <div className='grid md:grid-cols-2 gap-4'>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Name</h1><input type="text" placeholder="Name" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Name</h1><input type="text" required name='name' placeholder="Name" className="input input-bordered input-warning w-full" />
 </div>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Brand Name</h1><input type="text" placeholder="Brand Name" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Brand Name</h1><input type="text" required name='brand' placeholder="Brand Name" className="input input-bordered input-warning w-full" />
 </div>
 </div>
 <div className='grid md:grid-cols-2 gap-4'>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Type</h1><input type="text" placeholder="Type" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Type</h1><input type="text" required name='type' placeholder="Type" className="input input-bordered input-warning w-full" />
 </div>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Price</h1><input type="text" placeholder="Price" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Price</h1><input type="number" required name='price' placeholder="Price" className="input input-bordered input-warning w-full" />
 </div>
 </div>
 <div className='grid md:grid-cols-2 gap-4'>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Short description</h1><input type="text" placeholder="Short description" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Short description</h1><input type="text" required name='description' placeholder="Short description" className="input input-bordered input-warning w-full" />
 </div>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Rating</h1><input type="text" placeholder="Rating" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Rating</h1><input type="" required name='rating' placeholder="Rating" className="input input-bordered input-warning w-full" />
 </div>
 </div>
 <div className=''>
 <div className='space-y-2'>
-    <h1 className='font-semibold'>Photo</h1><input type="text" placeholder="Photo URL" className="input input-bordered input-warning w-full" />
+    <h1 className='font-semibold'>Photo</h1><input type="url" required name='photo' placeholder="Photo URL" className="input input-bordered input-warning w-full" />
 </div>
 </div>
 <div>
-    <button className='btn w-full text-white bg-orange-500 font-semibold'>Add Product</button>
+    <button type='submit' className='btn w-full text-white bg-orange-500 font-semibold'>Add Product</button>
 </div>
     </form>
 </div>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,15 +1,66 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { BsFillPenFill,BsEyeFill } from 'react-icons/bs';
+
 
 
 const Products = () => {
+
+    const [cars,setCars] = useState([])
+
     const params = useParams()
     // console.log( (params.id).toLowerCase());
     const brandName = params.id.toLowerCase()
     console.log(brandName);
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/${brandName}`)
+        .then(res => res.json())
+        .then(data => setCars(data))
+    },[])
     return (
+        <>
         <div>
-            <h1>Product list here</h1>
+            <h1>banner will be here</h1>
         </div>
+
+<div className="container mx-auto px-3 md:px-20">
+<h1 className="text-3xl md:text-5xl lg:text-7xl font-bold">Popular Makes</h1>
+
+<div className="grid lg:grid-cols-2 gap-5 my-20">
+
+{
+    cars.map(car => <div key={car._id} className="card bg-slate-700 card-compact w-full shadow-xl">
+    <figure><img className="w-full h-96" src={car.photo} alt="Car" /></figure>
+    <div className="card-body">
+      <h2 className="text-xl font-bold text-white">{car.name}</h2>
+      <p className="text-lg font-semibold text-white"><span className="font-bold text-orange-400">Brand Name: </span>{car.brandName}</p>
+      <p className="text-lg font-semibold text-white"><span className="font-bold text-orange-400">Type: </span>{car.type}</p>
+      <p className="text-lg font-semibold text-white"><span className="font-bold text-orange-400">Price: </span>${car.price}</p>
+      <div className="text-lg font-semibold text-white"><span className="font-bold text-orange-400">Rating: </span><div className="rating">
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked/> {car.rating}
+</div></div>
+
+<div>
+
+</div>
+
+      <div className="card-actions justify-between">
+        <button className="btn btn-primary flex items-center gap-3 text-white bg-orange-500 border-none"><BsEyeFill className="text-2xl"/> View Details</button>
+        <button className="btn btn-primary flex items-center gap-3 text-white bg-orange-500 border-none"><BsFillPenFill className="text-2xl"/> Update Product</button>
+      </div>
+    </div>
+  </div>)
+}
+
+</div>
+
+</div>
+        </>
     );
 };
 
