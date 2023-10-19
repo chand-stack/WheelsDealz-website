ea04cdffd87be2049651e72dc767dee4a9edf96f
chand-stack <chand.stack@gmail.com>
2023-10-19 06:43:46 +0600

    Chore: delete handler added

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,9 +1,31 @@
 import { useLoaderData } from 'react-router-dom';
 import bg from '../assets/carcover1.jpg'
+import Swal from 'sweetalert2';
+import { useState } from 'react';
 
 const Cart = () => {
     const data = useLoaderData()
-    console.log(data);
+    const [carts,setCarts] = useState(data)
+
+    const cartRemove = _id => {
+        console.log(_id);
+        fetch(`http://localhost:5000/cart/${_id}`,{
+            method:"DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount>0){
+                Swal.fire(
+                    'Great!',
+                    'Removed successfully!',
+                    'success'
+                  )
+                  const remaining = carts.filter(car => car._id !== _id)
+                  setCarts(remaining)
+            }
+        })
+    }
     return (
         <>
         <div className='h-[50vh]' style={{backgroundImage:`url(${bg})`,backgroundSize:"cover",backgroundRepeat:"no-repeat", backgroundPosition:"center"}}>
@@ -17,7 +39,7 @@ const Cart = () => {
 <div className=''>
 
 {
-    data.map(car => <div key={car._id} className='grid md:grid-cols-2 p-2 md:p-5'>
+    carts.map(car => <div key={car._id} className='grid md:grid-cols-2 p-2 md:p-5'>
         <img className='md:rounded-l-2xl' src={car.photo} alt="" />
         <div className='bg-slate-100 flex flex-col md:rounded-r-2xl p-3 hover:bg-black hover:text-white hover:duration-1000'>
 
@@ -31,7 +53,7 @@ const Cart = () => {
       <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
       <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" defaultChecked/> {car.rating}
     </div></div>
-    <div><button className="btn w-full text-white bg-orange-500 font-semibold">Delete</button></div>
+    <div><button onClick={()=> cartRemove(car._id)} className="btn w-full text-white bg-orange-500 font-semibold">Remove From Cart</button></div>
 
         </div>
     </div>)
