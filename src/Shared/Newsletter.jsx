734bb975f238a603ece030691a02bbb0ad3db16a
chand-stack <chand.stack@gmail.com>
2023-10-20 20:27:08 +0600

    Fix: newsletter section code fix

--- src/Shared/Newsletter.jsx
+++ src/Shared/Newsletter.jsx
@@ -2,23 +2,23 @@ import { Link } from "react-router-dom";
 
 const Newsletter = () => {
   return (
-    <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center my-10 gap-3">
-      <h1 className="text-3xl md:text-5xl font-bold">Newsletter</h1>
-      <div>
+    <div className="container mx-auto  my-10 space-y-5">
+      <h1 className="text-3xl md:text-5xl font-bold text-center">Newsletter</h1>
+      <div className="flex justify-center">
         <input
           type="text"
           placeholder="Your email address"
           className="input input-bordered input-lg w-[500px] max-w-xs"
         />
       </div>
-      <div>
+      <div className="flex justify-center">
         <Link to="/register">
           <button className="btn text-white bg-orange-500 btn-secondary px-12 border-none">
             Sign up
           </button>
         </Link>
       </div>
-      <p className="text-xl">
+      <p className="text-xl flex justify-center text-current">
         Subscribe to our newsletter and stay updated with our offer
       </p>
     </div>
